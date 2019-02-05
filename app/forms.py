705fea60558d728eedbd5d87aa7623d7585dabb4
rbedretdinov <rustem.bedretdinov@aciworldwide.com>
2019-02-05 22:46:03 +0300

    Fixed server-side validation on Login form

--- app/forms.py
+++ app/forms.py
@@ -1,29 +1,37 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, InputRequired, Email, EqualTo, Length
 from app.models import User
 
+
+def _required(form, field):
+    if not field.raw_data or not field.raw_data[0]:
+        raise ValidationError("Field {} is required".format(field.name))
+
+
 class LoginForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	remember_me = BooleanField('Remember Me', default=True)
-	submit = SubmitField('Sign In')
+    username = StringField('Username', validators=[_required, Length(5,64)])
+    password = PasswordField('Password', validators=[_required, Length(5, 32)])
+    remember_me = BooleanField('Remember Me', default=True)
+    submit = SubmitField('Sign In')
+
 
 class RegistrationForm(FlaskForm):
-	username = StringField('Username', validators=[DataRequired()])
-	firstname = StringField('First name')
-	lastname = StringField('Last name')
-	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Password', validators=[DataRequired()])
-	password2 = PasswordField('Password confirmation', validators=[DataRequired(), EqualTo('password')])
-	submit = SubmitField('Register')
+    username = StringField('Username', validators=[InputRequired()])
+    firstname = StringField('First name')
+    lastname = StringField('Last name')
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    password2 = PasswordField('Password confirmation', validators=[
+                              InputRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
 
-	def validate_username(self, username):
-		user = User.query.filter(User.username==username.data).first()
-		if user is not None:
-			raise ValidationError('Please enter other username')
+    def validate_username(self, username):
+        user = User.query.filter(User.username == username.data).first()
+        if user is not None:
+            raise ValidationError('Please enter other username')
 
-	def validate_email(self, email):
-		user = User.query.filter(User.email==email.data).first()
-		if user is not None:
-			raise ValidationError('Please enter other email')
+    def validate_email(self, email):
+        user = User.query.filter(User.email == email.data).first()
+        if user is not None:
+            raise ValidationError('Please enter other email')
