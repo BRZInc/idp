89c1ae1f914e525d612a4f620ea9601a9f708478
BRZInc <rustem.bedretdinov@gmail.com>
2019-02-12 15:14:46 +0300

    Fixed formatting of models.py

--- app/models.py
+++ app/models.py
@@ -3,39 +3,41 @@ from datetime import timedelta, datetime
 from werkzeug import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-@login.user_loader 
+
+@login.user_loader
 def load_user(id):
-	return User.query.get(int(id))
+    return User.query.get(int(id))
 
 
 class User(UserMixin, db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True, nullable=False)
-	first_name = db.Column(db.String(120))
-	last_name = db.Column(db.String(120))
-	email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-	password_hash = db.Column(db.String(128), nullable=False)
-	goals = db.relationship('Goal', backref='author', lazy='dynamic')
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True,
+                         unique=True, nullable=False)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(120))
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    goals = db.relationship('Goal', backref='author', lazy='dynamic')
 
-	def set_password(self, password):
-		self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-	def check_password(self, password):
-		return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
+    def __repr__(self):
+        return '<User {} id={}>'.format(self.username, self.id)
 
-	def __repr__(self):
-		return '<User {} id={}>'.format(self.username, self.id)
 
 class Goal(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(140), nullable=False)
-	description = db.Column(db.String(1024))
-	duedate = db.Column(db.DateTime, index=True)
-	created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow())
-	# TODO: Add update of updated_at field on every update
-	updated_at = db.Column(db.DateTime, default=datetime.utcnow())
-	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140), nullable=False)
+    description = db.Column(db.String(1024))
+    duedate = db.Column(db.DateTime, index=True)
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+    # TODO: Add update of updated_at field on every update
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-	def __repr__(self):
-		return '<Goal {} id={}>'.format(self.title, self.id)
+    def __repr__(self):
+        return '<Goal {} id={}>'.format(self.title, self.id)
